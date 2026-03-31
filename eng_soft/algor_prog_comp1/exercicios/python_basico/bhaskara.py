f13855799451f9de0cd925f9a12b75689cc0c41d
GuBoeing <197969463+gustavoboeing12@users.noreply.github.com>
2026-03-31 10:13:13 -0300

    Comentários no código

--- eng_soft/algor_prog_comp1/exercicios/python_basico/bhaskara.py
+++ eng_soft/algor_prog_comp1/exercicios/python_basico/bhaskara.py
@@ -1,15 +1,17 @@
 #
 # Programa para cálcular a fórmula de bhaskara
 
+# Coleta de dados
 a = int(input("Informe o valor de A: \n"))
 b = int(input("Informe o valor de B: \n"))
 c = int(input("Informe o valor de C: \n"))
 
+# Cálculos
 delta = (b**2)-(4*a*c)
-
 x1 = (-b + (delta**0.5)) / (2*a)
 x2 = (-b - (delta**0.5)) / (2*a)
 
+# Print dos resultados
 print(f"Valor de x1: {x1:.2f}")
 print(f"Valor de x2: {x2:.2f}")
 
