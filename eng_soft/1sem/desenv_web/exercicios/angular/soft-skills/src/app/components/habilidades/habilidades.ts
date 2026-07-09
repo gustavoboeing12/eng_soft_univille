7ef59e3fa8d7e04d0ea3277cb7928e99ef38a37c
GuBoeing <197969463+gustavoboeing12@users.noreply.github.com>
2026-07-09 20:17:40 -0300

    Trabalho web sobre Angular

---
+++ eng_soft/1sem/desenv_web/exercicios/angular/soft-skills/src/app/components/habilidades/habilidades.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habilidades',
+  imports: [],
+  templateUrl: './habilidades.html',
+  styleUrl: './habilidades.css',
+})
+export class Habilidades {}
